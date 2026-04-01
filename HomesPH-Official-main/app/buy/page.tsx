715c5cbc90af9d3a702dcf13af197a7e101b33a2
JAEVIE BAYONA <70928556+EshCookiez@users.noreply.github.com>
2026-04-01 17:14:51 +0800

    delete again

--- HomesPH-Official-main/app/buy/page.tsx
+++
@@ -1,9 +0,0 @@
-import PublicListingsPage from '@/components/listings/PublicListingsPage'
-import type { PropertySearchParamsInput } from '@/lib/property-search'
-
-export default async function BuyPage(props: {
-  searchParams?: Promise<PropertySearchParamsInput>
-}) {
-  const searchParams = (await props.searchParams) ?? {}
-  return <PublicListingsPage mode="sale" searchParams={searchParams} />
-}
